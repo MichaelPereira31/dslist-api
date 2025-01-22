82c5bebb3bd219d00d62ef59fe5482dc04656bb5
Michael Pereira <michaelpereira408@gmail.com>
2025-01-22 20:56:16 -0300

    feat: add entity game

---
+++ src/main/java/com/dev/dslist/entities/Game.java
@@ -0,0 +1,49 @@
+package com.dev.dslist.entities;
+
+import lombok.Setter;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "games")
+public class Game {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String title;
+  @Column(name = "game_year")
+  private Integer year;
+  private String genre;
+  private String platform;
+  private String imgUrl;
+  private String shortDescription;
+  private String longDescription;
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Game other = (Game) obj;
+    return Objects.equals(id, other.id);
+  }
+}
